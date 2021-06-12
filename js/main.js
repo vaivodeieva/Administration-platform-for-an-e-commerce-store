c45235938cec161eb728a5daddf4ab03a804a5ad
Katrina M <merca3@inbox.lv>
2021-06-12 13:25:20 +0300

    JS_cleaned_up

--- js/main.js
+++ js/main.js
@@ -1,23 +1,14 @@
 $(document).ready(function() {
-    $('#products-datatable').DataTable();
+    $('#products-datatable, #orders-datatable, #categories-datatable').DataTable();
 });
 
-$(document).ready(function() {
-    $('#orders-datatable').DataTable();
+$('#products-datatable, #orders-datatable, #categories-datatable').dataTable({
+    "lengthMenu": [
+        [5, 10, 15, -1],
+        [5, 10, 15, "All"]
+    ]
 });
 
-$(document).ready(function() {
-    $('#categories-datatable').DataTable();
-});
-
-$('.delete-product').click(function() {
+$('.delete-product, .delete-order, .btn-del').click(function() {
     $(this).closest('tr').remove();
 })
-
-$('.delete-order').click(function() {
-    $(this).closest('tr').remove();
-})
-
-$('.btn-del').click(function() {
-    $(this).closest('tr').remove();
-});
